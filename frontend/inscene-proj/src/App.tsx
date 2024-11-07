bb080235f6625ee82a4cb6701e06205fb5ba3fba
suphanatchanlek <suphanatchanlek@gmail.com>
2024-11-07 23:19:02 +0700

    add Navbar

--- frontend/inscene-proj/src/App.tsx
+++ frontend/inscene-proj/src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  
   return (
-    <div>test</div>
-  )
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <Navbar />
+      {/* Other components */}
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/inscene-proj/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile_user from '../../components/Profile_user'
+
+const Home = () => {
+  return (
+    <div className='p-4'>
+    <Profile_user/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi incidunt hic facere officiis impedit ex ullam! Dolorem debitis similique quae.</p>
+    </div>
+  )
+}
+
+export default Home
