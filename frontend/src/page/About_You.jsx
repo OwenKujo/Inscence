67afdecadd97a4309bba5231d0f4d5243c9b800c
popolin47 <popo2027188@gmail.com>
2024-11-13 00:20:37 +0700

    detail page aboutU

--- frontend/src/page/About_You.jsx
+++ frontend/src/page/About_You.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa'; // Import the left arrow icon
+import { useNavigate } from 'react-router-dom';
+import bg from '../assets/Detail/AboutU_bg.png';
+import Box from '../assets/Detail/Laem_Ka_Beach.png';
 
 const About_You = () => {
-  return (
-    <div>Test Page About_You</div>
-  )
-}
+  const navigate = useNavigate(); // Hook for navigation
 
-export default About_You
+  const handleBackClick = () => {
+    navigate('/'); // Navigate to Home page
+  };
+  return (
+    <div
+      className="h-screen w-full bg-cover bg-center flex flex-col items-center justify-center relative"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      {/* Back Icon */}
+      <div
+        className="absolute top-5 left-5 text-white text-3xl cursor-pointer"
+        onClick={handleBackClick} // Attach the click handler to navigate back
+      >
+        <FaArrowLeft />
+      </div>
+
+      <h1 className="text-white text-3xl font-bold font-elegant ">
+        I Told Sunset About You
+      </h1>
+
+      <div className="w-full flex justify-center mt-72">
+        <img
+          src={Box}
+          alt="Box below text"
+          className="w-10/12 h-auto object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default About_You;
