2e935f1a962d975bfefd2541f6d25a201c8d0aa5
Owen Kujo <rapeepat.cadflow@gmail.com>
2024-11-17 15:47:03 +0700

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,5 @@
 import { User } from "../models/userModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import TryCatch from "../utils/TryCatch.js";
 import generateToken from "../utils/generateToken.js";
 
