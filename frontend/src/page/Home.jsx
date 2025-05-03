a827c5b5cc64a0212ac8c07b6a0d55d10c5d274e
suphanatchanlek <suphanatchanlek@gmail.com>
2025-05-03 23:12:39 +0700

    add Swiper

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -2,14 +2,20 @@
 import Profile_user from '../components/Profile_user'
 import Card_recommend from '../components/Card_recommend'
 import Card_All_show from '../components/Card_All_show'
+import Hero from './Hero'
 
 const Home = () => {
   return (
     <div>
+      {/* Swiper */}
+      <div className='w-[350px] h-[200px] bg-[#000000] relative justify-center mx-auto mt-[100px] rounded-[20px] overflow-hidden'>
+        <Hero/>
+      </div>
+
       {/* user */}
       {/* <Profile_user /> */}
       {/* Recommend */}
-      <div className='mt-[60px] p-4'>
+      <div className='mt-[20px] p-4'>
           <h1 className='text-[20px] font-body font-bold text-[#000000] mb-4'>
             Recommend Movies categories
           </h1>
