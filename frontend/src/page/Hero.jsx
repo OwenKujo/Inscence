f03924565debf2822f6b466721c1566e5edec1f1
suphanatchanlek <suphanatchanlek@gmail.com>
2025-05-05 19:59:43 +0700

    update Swiper add Image

--- frontend/src/page/Hero.jsx
+++ frontend/src/page/Hero.jsx
@@ -11,6 +11,18 @@ import 'swiper/css/navigation';
 
 import '../styles/styles.css';
 
+// import image
+import Image1 from '../assets/Image_Swiper/Image_1.jpg';
+import Image2 from '../assets/Image_Swiper/Image_2.jpg';
+import Image3 from '../assets/Image_Swiper/Image_3.jpg';
+import Image4 from '../assets/Image_Swiper/Image_4.jpg';
+import Image5 from '../assets/Image_Swiper/Image_5.jpg';
+import Image6 from '../assets/Image_Swiper/Image_6.jpg';
+import Image7 from '../assets/Image_Swiper/Image_7.jpg';
+import Image8 from '../assets/Image_Swiper/Image_8.png';
+import Image9 from '../assets/Image_Swiper/Image_9.jpg';
+
+
 // import required modules
 import { Autoplay, Pagination } from 'swiper/modules';
 
@@ -31,15 +43,33 @@ const Hero = () => {
         modules={[Autoplay, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <img src={Image2} alt="Image 1" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image1} alt="Image 2" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image3} alt="Image 3" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image4} alt="Image 4" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image5} alt="Image 5" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image6} alt="Image 6" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image9} alt="Image 7" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image8} alt="Image 8" className='w-full h-full object-cover' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Image7} alt="Image 9" className='w-full h-full object-cover' />
+        </SwiperSlide>
       </Swiper>
     </>
   );
